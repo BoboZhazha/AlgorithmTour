990c602dfc8fdccc74229a1b49665e944ab8bde0
zhang <909319019@qq.com>
2020-01-03 14:17:17 +0800

    add merge bubblesort

--- sort-advance/src/BubbleSort.java
+++ sort-advance/src/BubbleSort.java
@@ -1,2 +1,34 @@
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class BubbleSort {
+
+    public static void main(String[] args) {
+        int arr[] = {8, 2, 3, 45, 6, 7};
+        sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    //比较相邻的元素。如果第一个比第二个大，就交换他们两个
+    public static void sort(int arr[]) {
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp;
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
 }
