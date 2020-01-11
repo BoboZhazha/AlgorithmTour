36162fcae3557e90a55abd5e3888cbe764c8cc00
zhang <909319019@qq.com>
2020-01-11 12:48:26 +0800

    添加重构版的bst, 抽象出二叉树接口

--- bst/src/BinarySearchTree.java
+++ bst/src/BinarySearchTree.java
@@ -1,8 +1,6 @@
-import javax.swing.*;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.logging.Level;
 
 public class BinarySearchTree<E> {
 
@@ -29,6 +27,14 @@ public class BinarySearchTree<E> {
             this.element = element;
             this.parent = parent;
         }
+
+        public boolean isLeaf() {
+            return left == null && right == null;
+        }
+
+        public boolean hasTwoChildren() {
+            return left != null && right != null;
+        }
     }
 
 
@@ -36,28 +42,28 @@ public class BinarySearchTree<E> {
      * 前序遍历
      */
     public void preorderTraversal(Visitor<E> visitor) {
-        preorderTraversal(root,visitor);
+        preorderTraversal(root, visitor);
     }
 
-    private void preorderTraversal(Node<E> node,Visitor<E> visitor) {
+    private void preorderTraversal(Node<E> node, Visitor<E> visitor) {
         if (node == null) return;
         visitor.visit(node.element);
-        preorderTraversal(node.left,visitor);
-        preorderTraversal(node.right,visitor);
+        preorderTraversal(node.left, visitor);
+        preorderTraversal(node.right, visitor);
     }
 
     /**
      * 中序遍历
      */
     public void inorderTraversal(Visitor<E> visitor) {
-        postorderTraversal(root,visitor);
+        postorderTraversal(root, visitor);
     }
 
-    private void inorderTraversal(Node<E> node,Visitor<E> visitor) {
+    private void inorderTraversal(Node<E> node, Visitor<E> visitor) {
         if (node == null) return;
-        inorderTraversal(node.left,visitor);
+        inorderTraversal(node.left, visitor);
         visitor.visit(node.element);
-        inorderTraversal(node.right,visitor);
+        inorderTraversal(node.right, visitor);
     }
 
     public static interface Visitor<E> {
@@ -69,20 +75,19 @@ public class BinarySearchTree<E> {
      * 后序遍历
      */
     public void postorderTraversal(Visitor<E> visitor) {
-        postorderTraversal(root,visitor);
+        postorderTraversal(root, visitor);
     }
 
-    private void postorderTraversal(Node<E> node,Visitor<E> visitor) {
+    private void postorderTraversal(Node<E> node, Visitor<E> visitor) {
         if (node == null) return;
-        postorderTraversal(node.left,visitor);
-        postorderTraversal(node.right,visitor);
+        postorderTraversal(node.left, visitor);
+        postorderTraversal(node.right, visitor);
         visitor.visit(node.element);
     }
 
 
     /**
      * 层序遍历
-     *
      */
     public void levelTraveral(Visitor<E> visitor) {
         if (root == null) return;
@@ -102,7 +107,7 @@ public class BinarySearchTree<E> {
 
     /**
      * 获得二叉树的高度
-     * @return
+     *
      */
     public int height() {
         return height(root);
@@ -110,8 +115,7 @@ public class BinarySearchTree<E> {
 
     /**
      * 递归方式
-     * @param node
-     * @return
+     *
      */
     private int height(Node<E> node) {
         if (node == null) {
@@ -122,7 +126,7 @@ public class BinarySearchTree<E> {
 
     /**
      * 非递归方式获得高度, 通过层级遍历
-     * @return
+     *
      */
     public int height2() {
         if (root == null) return 0;
@@ -212,9 +216,6 @@ public class BinarySearchTree<E> {
     /**
      * 外面传了比较实现了比较方法的接口就使用,不传就用默认的
      *
-     * @param e1
-     * @param e2
-     * @return
      */
     private int compare(E e1, E e2) {
         // 如果你有comparator那么就用你的
@@ -228,28 +229,105 @@ public class BinarySearchTree<E> {
 
     /**
      * 传入一个元素,删除这个节点,外部调用者是不知道节点这个东西存在的,只需要传入元素,她
-     * @param element
+     *
      */
     public void remove(E element) {
         remove(node(element));
         return;
     }
 
+
+
     private void remove(Node<E> node) {
-        if (node==null) return;
+        if (node == null) return;
 
         size--;
+
+        if (node.hasTwoChildren()) { // 度为2的节点
+            // 找到后继节点
+            Node<E> s = successor(node);
+            // 用后继节点的值覆盖度为2的节点的值
+            node.element = s.element;
+            // 删除后继节点
+            node = s;
+        }
+
+        // 删除node节点（node的度必然是1或者0）
+        Node<E> replacement = node.left != null ? node.left : node.right;
+
+        if (replacement != null) { // node是度为1的节点
+            // 更改parent
+            replacement.parent = node.parent;
+            // 更改parent的left、right的指向
+            if (node.parent == null) { // node是度为1的节点并且是根节点
+                root = replacement;
+            } else if (node == node.parent.left) {
+                node.parent.left = replacement;
+            } else { // node == node.parent.right
+                node.parent.right = replacement;
+            }
+        } else if (node.parent == null) { // node是叶子节点并且是根节点
+            root = null;
+        } else { // node是叶子节点，但不是根节点
+            if (node == node.parent.left) {
+                node.parent.left = null;
+            } else { // node == node.parent.right
+                node.parent.right = null;
+            }
+        }
     }
+
+
+    // 前驱节点:中序遍历时的前一个节点(左子树里的最大节点)
+    // 如果左子树不为空,则从左子树里找到最右右右为止
+    private Node<E> predesessor(Node<E> node) {
+        if (node == null) return null;
+
+        // 左边不为空,从左子树去找
+        Node<E> p = node.left;
+        if (p != null) {
+            while (p.right != null) {
+                p = p.right;
+            }
+            return p;
+
+        }
+        // 从父节点,祖父节点中找到前驱节点
+        while (node.parent != null && node == node.parent.left) {
+            node = node.parent;
+        }
+        return node.parent;
+
+    }
+
+    //  找后继节点
+    private Node<E> successor(Node<E> node) {
+        if (node == null) return null;
+
+        // 左边不为空,从左子树去找
+        Node<E> p = node.right;
+        if (p != null) {
+            while (p.left != null) {
+                p = p.left;
+            }
+            return p;
+        }
+        while (node.parent != null && node == node.parent.right) {
+            node = node.parent;
+        }
+        return node.parent;
+
+    }
+
+
 
     /**
      * 传入一个元素,找到这个节点
-     * @param element
-     * @return
      */
     private Node<E> node(E element) {
         Node<E> node = root;
         while (node != null) {
-            int cmp = compare(element,node.element);
+            int cmp = compare(element, node.element);
             if (cmp == 0) return node;
             if (cmp > 0) {
                 node = node.right;
